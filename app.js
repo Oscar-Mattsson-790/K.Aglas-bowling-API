5110ff71955f23780cb6c716234d9471cbdffa7f
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-05-29 10:37:01 +0200

    boiler plate

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const bowlingRouter = require("./routes/bowling");
+
+app.use(express.json());
+
+app.use("/api", bowlingRouter); // Mount the bowling router at /api
+
+try {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+} catch (error) {
+  console.log("Failed to connect to the database:", error);
+}
---
+++ models/db.js
@@ -0,0 +1,31 @@
+// models/db.js
+const sqlite3 = require("sqlite3").verbose();
+
+function createDbConnection() {
+  const db = new sqlite3.Database("./bowling.sqlite", (error) => {
+    if (error) return console.log(error.message);
+    createTable(db);
+  });
+
+  return db;
+}
+
+function createTable(db) {
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS bookings (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      date TEXT,
+      email TEXT,
+      time TEXT,
+      numPeople INTEGER,
+      numCourses INTEGER,
+      shoeSizes TEXT,
+      totalPrice INTEGER,
+      bookingNumber TEXT
+    )
+  `);
+}
+
+const db = createDbConnection();
+
+module.exports = db;
