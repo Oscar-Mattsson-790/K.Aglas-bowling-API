48a64643a7118ecdf916f280169c9416515b3e66
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-06-01 12:34:49 +0200

    complete

--- routes/createBooking.js
+++ routes/createBooking.js
@@ -1,63 +1,93 @@
 const express = require("express");
-const { db, checkBookingNumberExists } = require("../models/db");
+const {
+  db,
+  checkBookingNumberExists,
+  checkCourseAvailability,
+} = require("../models/db");
 
 const router = express.Router();
 
+// Helper function to generate a booking number
 async function generateBookingNumber() {
   const bookingNumber = Math.floor(Math.random() * 90000) + 10000;
   const exists = await checkBookingNumberExists(bookingNumber);
 
   if (exists) {
-    return await generateBookingNumber();
+    return await generateBookingNumber(); // If the generated booking number exists, generate again
   }
 
   return bookingNumber;
 }
 
+// Create a booking
 router.post("/", async (req, res) => {
   const { date, email, time, numPeople, numCourses, shoeSizes } = req.body;
 
+  // Calculate the total price
   const totalPrice = numPeople * 120 + numCourses * 100;
 
+  // Generate a booking number
   const bookingNumber = await generateBookingNumber();
 
-  db.run(
-    `INSERT INTO bookings (date, email, time, numPeople, numCourses, totalPrice, bookingNumber, shoeSizes)
-     VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-    [
-      date,
-      email,
-      time,
-      numPeople,
-      numCourses,
-      totalPrice,
-      bookingNumber,
-      shoeSizes,
-    ],
-    function (err) {
-      if (err) {
-        console.log(err);
-        res.status(500).json({ error: "Failed to create booking" });
-      } else {
-        const bookingId = this.lastID;
+  try {
+    // Check course availability
+    const isCourseAvailable = await checkCourseAvailability(date, time);
 
-        for (let i = 0; i < numCourses; i++) {
-          db.run(
-            `INSERT INTO courses (bookingId) VALUES (?)`,
-            [bookingId],
-            function (err) {
-              if (err) {
-                console.log(err);
-                res.status(500).json({ error: "Failed to create booking" });
+    if (!isCourseAvailable) {
+      // Insert the booking into the database
+      db.run(
+        `INSERT INTO bookings (date, email, time, numPeople, numCourses, totalPrice, bookingNumber, shoeSizes)
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+        [
+          date,
+          email,
+          time,
+          numPeople,
+          numCourses,
+          totalPrice,
+          bookingNumber,
+          shoeSizes,
+        ],
+        function (err) {
+          if (err) {
+            console.log(err);
+            return res.status(500).json({ error: "Failed to create booking" });
+          }
+
+          const bookingId = this.lastID;
+
+          // Insert the courses into the database
+          for (let i = 0; i < numCourses; i++) {
+            db.run(
+              `INSERT INTO courses (bookingId) VALUES (?)`,
+              [bookingId],
+              function (err) {
+                if (err) {
+                  console.log(err);
+                  return res
+                    .status(500)
+                    .json({ error: "Failed to create booking" });
+                }
               }
-            }
-          );
-        }
+            );
+          }
 
-        res.status(201).json({ bookingId });
-      }
+          res.status(201).json({ bookingId });
+        }
+      );
+    } else {
+      res
+        .status(400)
+        .json({
+          error: "Course already booked at the requested date and time",
+        });
     }
-  );
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: "An error occurred while checking course availability" });
+  }
 });
 
 module.exports = router;
--- routes/updateBooking.js
+++ routes/updateBooking.js
@@ -4,18 +4,21 @@ const { checkBooking, verifyBookingNumber } = require("../middleware/bowling");
 
 const router = express.Router();
 
+// Update a booking
 router.put("/:bookingNumber", checkBooking, verifyBookingNumber, (req, res) => {
   const { bookingNumber } = req.params;
   const { numPeople, numCourses, shoeSizes } = req.body;
 
+  // Check if the booking exists
   db.get(
-    `SELECT * FROM bookings WHERE bookingNumber = ?`,
+    "SELECT * FROM bookings WHERE bookingNumber = ?",
     [bookingNumber],
     (err, row) => {
       if (err) {
         console.log(err);
         res.status(500).json({ error: "Failed to update booking" });
       } else if (row) {
+        // Update the booking with new values
         const updatedBooking = {
           ...row,
           numPeople,
@@ -23,6 +26,7 @@ router.put("/:bookingNumber", checkBooking, verifyBookingNumber, (req, res) => {
           shoeSizes,
         };
 
+        // Check if the number of shoes matches the number of people
         const sizes = updatedBooking.shoeSizes.split(",").map(Number);
         if (sizes.length !== updatedBooking.numPeople) {
           res
@@ -31,8 +35,10 @@ router.put("/:bookingNumber", checkBooking, verifyBookingNumber, (req, res) => {
           return;
         }
 
+        // Recalculate the total price based on the updated values
         const totalPrice = calculateTotalPrice(updatedBooking);
 
+        // Update the booking in the database
         db.run(
           `UPDATE bookings
            SET numPeople = ?, numCourses = ?, shoeSizes = ?, totalPrice = ?
